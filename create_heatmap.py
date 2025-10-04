5f2499801d23b66ac936f7b98c26616fd22288dd
Julia Kauppinen <julia-jultsu@hotmail.com>
2025-10-04 17:00:43 +0300

    asd

--- create_heatmap.py
+++ create_heatmap.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import matplotlib.axes as axes
 import numpy as np
-
+from skimage import io, transform
 import matplotlib
 import matplotlib as mpl
 
 def create_heatmap():
-    data = np.zeros((80, 80))
+    data = np.zeros((80, 80)) # do not change
     data [0, 0] += 8
     data [1, 0] += 8
     data [0, 2] += 8
@@ -18,19 +18,20 @@ def create_heatmap():
     data [18,12] += 4
     data [18,13] += 4
 
+   # Create colormap mapping using matplotlib
+    cmap = plt.cm.spring
+    norm = mpl.colors.Normalize(vmin=data.min(), vmax=data.max())
+    
+    # Apply colormap to get RGBA image
+    rgba_data = cmap(norm(data))
+    
+    # Resize from 80x80 to 800x800 using skimage
+    resized_data = transform.resize(rgba_data, (800, 800), order=0, preserve_range=True)
+    
+    rgb_data = (resized_data[:, :, :3] * 255).astype(np.uint8)
 
-    fig, ax = plt.subplots(figsize=(800/300, 800/300), dpi=300)
-    ax.axis('off')
-    #ax.set_axis_off()
-    #if __name__ == "__main__":
-    im = ax.imshow(data, cmap="spring")
+    io.imsave("hack-demo-61n/static/figs/test.png", rgb_data)
 
-
-    # Loop over data dimensions and create text annotations.
-    fig.tight_layout()
-    # plt.figure(figsize=(800/300, 800/300), dpi=300)
-    plt.savefig("static/figs/test.png", transparent=True, bbox_inches=0)
-    #plt.show()
 
 if __name__ == "__main__":
     create_heatmap()
